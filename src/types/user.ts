cc9c7c52097999c98e502dc60c2c2b5c6336b6ef
William <liuyiliuyi6116@gmail.com>
2024-05-09 23:05:25 +0800

    types

---
+++ src/types/user.ts
@@ -0,0 +1,17 @@
+// User Information
+export type UserInfo = {
+  account: string
+  type: string
+  user_id: number
+  username: string
+  isLogin?: boolean
+} | null
+
+export type UserAction = {
+  type: string
+  info?: UserInfo
+}
+
+export type UserState = {
+  user: UserInfo
+}
