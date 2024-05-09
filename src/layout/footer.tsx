9ab600eee8feb3b87e6d749463b2c11c092546ff
William <liuyiliuyi6116@gmail.com>
2024-05-09 23:04:28 +0800

    layout

---
+++ src/layout/footer.tsx
@@ -0,0 +1,29 @@
+import { Layout } from "antd";
+const { Footer } = Layout;
+
+export default function BottomFooter() {
+  return (
+    <Footer className="footer">
+      <p>
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+        >
+          
+        </a>
+      </p>
+      <p>
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+        >
+          
+        </a>
+      </p>
+      <p></p>
+      <p>react-ant-admin</p>
+    </Footer>
+  );
+}
